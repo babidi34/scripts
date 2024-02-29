bc4cbf16383cb71d6560742da7cce4f87819a873
babidi34 <karimbaidi@gmail.com>
2024-02-29 14:31:57 +0100

    add test_code_http.sh

---
+++ test_code_http.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Vérifie si le nombre d'arguments est correct
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <URL> <Expected HTTP Status Code>"
+  exit 1
+fi
+
+# Stocke les arguments dans des variables pour plus de clarté
+URL="$1"
+EXPECTED_CODE="$2"
+
+# Effectue la requête et stocke le code de réponse HTTP
+response=$(curl -k -s -o /dev/null -w "%{http_code}" "$URL")
+echo "HTTP Response Code: $response"
+
+# Compare le code de réponse au code attendu
+if [[ "$response" -ne "$EXPECTED_CODE" ]]; then
+  echo "Failure: The response code is not $EXPECTED_CODE."
+  exit 1
+else
+  echo "Success: The server responded with a $EXPECTED_CODE code."
+fi
+
